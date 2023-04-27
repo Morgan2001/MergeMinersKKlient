9595711db3b88d6bf8ee6b933f3b71d3ecc6250d
kirill.virich <kirill.virich@gmail.com>
2023-04-27 18:53:14 +0700

    MergeMiners.Fun

--- Assets/Scripts/GameCore/Preloader/RestClient.cs
+++ Assets/Scripts/GameCore/Preloader/RestClient.cs
@@ -13,7 +13,7 @@ namespace GameCore.Preloader
 {
     public class RestClient
     {
-        private const string _baseUrl = "http://192.168.0.95:5208/";
+        private const string _baseUrl = "http://mergeminers.fun/";
         
         private readonly JsonSerializerSettings _settings;
 
